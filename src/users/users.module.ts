61aabdbb3317d5570fe65933ee7bbc77d8908d6f
Avesh-Kumar <youremail@example.com>
2024-01-04 14:39:18 +0530

    implemented middleware at global level and feature module level

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './models/users.model';
+import { LoggerMiddleware } from 'src/shared/middlewares/logger.middleware';
 
 @Module({
   imports: [MongooseModule.forFeature([{
@@ -12,4 +13,12 @@ import { UserSchema } from './models/users.model';
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      // .exclude({ path: '/user/:id', method: RequestMethod.GET })
+      .forRoutes(UsersController)
+
+  }
+}
