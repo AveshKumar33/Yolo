e7013d0102f84adf99e3fed731e17fbf55c85283
Avesh-Kumar <youremail@example.com>
2024-01-04 10:53:54 +0530

    implement create user and get user by user id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,58 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, InternalServerErrorException, NotAcceptableException, NotFoundException, Param, Post, UnauthorizedException } from '@nestjs/common';
 import { UserDto } from './dtos/users.dto';
 import { UsersService } from './users.service';
 
 @Controller('user')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
+
+    /**save users in the database **/
     @Post('create')
-    signup(@Body() userDto: UserDto) {
-        console.log('signupDto created', userDto);
-        return this.usersService.signup(userDto)
+    @HttpCode(201)
+    async createUser(@Body() userDto: UserDto) {
+        try {
+            const user = await this.usersService.createUser(userDto);
+            console.log('user created ', user)
+            if (user.id)
+                return user;
+            throw new NotAcceptableException()
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_ACCEPTABLE,
+                error: 'This user already available',
+            }, HttpStatus.NOT_ACCEPTABLE, {
+                cause: error
+            });
+        }
     }
+
+    /**get user by user id**/
+    @Get(':id')
+    @HttpCode(200)
+    async findUserById(@Param('id') id: string) {
+        try {
+            const user = await this.usersService.getUserById(id);
+            console.log('user found', user);
+            if (user)
+                return user;
+            throw new NotFoundException()
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: 'This user id is not available',
+            }, HttpStatus.NOT_FOUND, {
+                cause: error
+            });
+        }
+    }
+
     @Get('health')
     health() {
-        return 'server is running and running on the server'
+        throw new UnauthorizedException();
+    }
+
+    @Get('ab*cd')
+    findAll() {
+        return 'This route uses a wildcard';
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,4 @@
-// import { Inject, Injectable } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
-
-// @Injectable()
-// export class UsersService {
-//     constructor(@Inject(ConfigService) public configService: ConfigService) { }
-//     getHello(): string {
-
-//         const uri = this.configService.get<string>('DATABASE_USER')
-//         console.log('MONGOD sss', uri);
-//         console.log('All Config:', this.configService.get<string>('MONGO_URI'));
-//         return 'Hello World! users';
-//     }
-// }
-
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 // import * as bcrypt from 'bcrypt';
@@ -25,7 +10,9 @@ const saltOrRounds = 10;
 @Injectable()
 export class UsersService {
     constructor(@InjectModel("User") private userModel: Model<UserModel>) { }
-    async signup(user: User) {
+
+    /**save new user in to database **/
+    async createUser(user: User) {
         const newUser = new this.userModel({
             userName: user.userName,
             email: user.email,
@@ -34,10 +21,19 @@ export class UsersService {
             // password: await bcrypt.hash(user.password, 10)
         })
         try {
-            await newUser.save()
-            return 'signup successfully'
+            return await newUser.save()
         } catch (error) {
             console.log('Sign up failed', error);
+            return error;
+        }
+    }
+
+    /**get user by id from database **/
+    async getUserById(userId: string): Promise<User> {
+        try {
+            return await this.userModel.findById({ _id: userId });
+        } catch (error) {
+            return error;
         }
     }
 }
