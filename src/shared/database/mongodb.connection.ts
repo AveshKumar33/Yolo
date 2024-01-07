9835eff7ae20bc47052136428bf657b939f47088
AveshKumar33 <katiyaravesh333@gmail.com>
2024-01-07 13:37:15 +0530

    print mongo string

--- src/shared/database/mongodb.connection.ts
+++ src/shared/database/mongodb.connection.ts
@@ -10,7 +10,7 @@ export class MongooseConfigService implements MongooseOptionsFactory {
             const username = this.configService.get<string>('DATABASE_USER')
             const databasename = this.configService.get<string>('DATABASE_NAME')
             const password = this.configService.get<string>('DATABASE_PASSWORD')
-            // console.log(`mongodb+srv://${username}:${password}@cluster0.jbyq6le.mongodb.net/${databasename}?retryWrites=true&w=majority`)
+            console.log(`mongodb+srv://${username}:${password}@cluster0.jbyq6le.mongodb.net/${databasename}?retryWrites=true&w=majority`)
             return {
                 uri: `mongodb+srv://${username}:${password}@cluster0.jbyq6le.mongodb.net/${databasename}?retryWrites=true&w=majority`,
             }
